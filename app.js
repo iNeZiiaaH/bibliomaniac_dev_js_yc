63aa2b46f42f3a6dc8925aec4463773e79f8d53b
lucio.dellafelice <lucio.dellafelice@origins-digital.com>
2024-10-24 13:09:11 +0200

    (feat) bookdetail: Allow to show details by clicking on a particular book

--- app.js
+++ app.js
@@ -5,21 +5,10 @@ async function allBooks (query) {
         const response = await fetch(`${API_URL}${query}`);
         const data = await response.json();
         return data.items || [];
+        console.log(data.items);
     } catch (error) {
          console.error('erreur lors de la récuperation', error);
          return [];
-    }
-}
-
-async function getDetails (bookId) {
-    try {
-        const API_URL_DETAIL = `https://www.googleapis.com/books/v1/volumes/${bookId}`;
-        const response = await fetch(API_URL_DETAIL);
-        if(!response) {
-            throw new Error('Error obtaining the book details')
-        }
-    } catch (error) {
-        console.log('Error connecting with the API')
     }
 }
 
@@ -35,6 +24,7 @@ function resultsBooks(books) {
     books.forEach(book => {
         const bookDiv = document.createElement('div');
         bookDiv.classList.add('book');
+        const bookId = book.id;
 
         const title = book.volumeInfo.title || 'Titre non disponible';
         const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(',') : 'autheur non disponible';
@@ -46,6 +36,10 @@ function resultsBooks(books) {
             <p>Auteur : ${authors}</p>`;
 
         result.appendChild(bookDiv);
+
+        bookDiv.addEventListener('click', ()=> {
+            window.location.href = `one-book.html?bookId=${bookId}`;
+        });
 
     })
 }
@@ -62,3 +56,46 @@ document.getElementById('searchForm').addEventListener('submit', async function(
         document.getElementById('results').innerHTML = '<p>Entrez une recherche.</p>';
     }
 });
+
+function getBookId() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('bookId');
+}
+
+async function getDetails (bookId) {
+    try {
+        const API_URL_DETAIL = `https://www.googleapis.com/books/v1/volumes/${bookId}`;
+        const response = await fetch(API_URL_DETAIL);
+        if(!response) {
+            throw new Error('Error obtaining the book details')
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('Error connecting with the API')
+    }
+}
+
+async function showDetails () {
+    const BookId= getBookId();
+    const bookDetails = await getDetails(BookId);
+    const bookDetailDiv = document.getElementById('bookDetails')
+
+    if(bookDetails) {
+        const title = bookDetails.volumeInfo.title || 'Titre non disponible';
+        const authors = bookDetails.volumeInfo.authors ? bookDetails.volumeInfo.authors.join(',') : 'autheur non disponible';
+        const img = bookDetails.volumeInfo.imageLinks ? bookDetails.volumeInfo.imageLinks.medium : '';
+        const description = bookDetails.volumeInfo.description || 'Description non disponible';
+        const publishedDate = bookDetails.volumeInfo.publishedDate || 'Date de publication non disponible';
+
+        bookDetailDiv.innerHTML = `
+            ${img ? `<img src="${img}" alt="${title}">`: ''}
+            <h2>${title}</h2>
+            <p>Autheur : ${authors}</p>
+            <p>Date de publication : ${publishedDate}</p>
+            <p>Description : ${description}</p>
+            `;
+    } else {
+        bookDetailDiv.innerHTML = '<p>Erreur lors de la récupération des détails du livre.</p>';
+    }
+}
